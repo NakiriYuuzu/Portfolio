5c25f802e471791f5824090439707a9a26979294
herrytsai <herrytsai@gis.tw>
2024-08-08 15:17:45 +0800

    🪠BugFix: Fix template broken css and broken javascript.

--- kotlin-js-template/device.js
+++ kotlin-js-template/device.js
@@ -1,16 +1,12 @@
-/* Storing user's device details in a variable*/
-const details = navigator.userAgent
+const isAppleDeviceOrSafari = () => {
+    const userAgent = navigator.userAgent.toLowerCase()
+    const isSafari = userAgent.includes('safari') && !userAgent.includes('chrome')
+    const isAppleDevice = /iphone|ipad|ipod|macintosh|mac os x/.test(userAgent)
+    const isAppleSilicon = /macintosh.+(apple|arm)/.test(userAgent)
+    return isSafari || isAppleDevice || isAppleSilicon
+}
 
-/* Creating a regular expression
-containing some mobile devices keywords
-to search it in details string*/
-const regexp = /iphone|kindle|ipad|Safari|AppleWebKit/i
-
-/* Using test() method to search regexp in details
-it returns boolean value*/
-const isAppleDevice = regexp.test(details)
-
-if (isAppleDevice) {
+if (isAppleDeviceOrSafari()) {
     document.write('<div class="container"> <object class="internal" type="text/html" data="js/index.html" ></object> </div>')
 } else {
     document.write('<div class="container"> <object class="internal" type="text/html" data="wasmJs/index.html" ></object> </div>')
