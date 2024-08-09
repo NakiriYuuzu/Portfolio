09eed3a45a1d8bdfdb1fbfbad100eb18ac1db2c6
herrytsai <herrytsai@gis.tw>
2024-08-09 16:21:43 +0800

    🪠BugFix: Fixed an issue where UI elements did not update correctly after switching themes.

--- composeApp/src/commonMain/kotlin/feature/di/ViewModelModule.kt
+++ composeApp/src/commonMain/kotlin/feature/di/ViewModelModule.kt
@@ -1,10 +1,9 @@
 package feature.di
 
 import feature.setting.SettingViewModel
-import org.koin.core.module.dsl.factoryOf
-import org.koin.core.module.dsl.viewModelOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 internal val viewModelModule = module {
-    viewModelOf(::SettingViewModel)
+    singleOf(::SettingViewModel)
 }
